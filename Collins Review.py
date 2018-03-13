a6391ababdbab1d37f31848e0c2642b681863f71
Collins M Holmes <collins.holmes@stu.jefferson.kyschools.us>
2018-03-13 09:28:06 -0400

    First commit

---
+++ Collins Review.py
@@ -0,0 +1,33 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> new_list= []
+print new_list
+
+Game_one ='Uno'
+print (Game_one)
+
+print ('or')
+
+Game_Two='War'
+print (Game_Two) 
+
+Video_Game = str(input ("Uno or War?:"))
+if Video_Game == Game_Two:
+    print ('Uno is the better game')
+else:
+    print ('War is the better game')
+     
+print ('**********************')
+    
+if Video_Game == Game_Two:
+    print ('War is played with normal playing cards')
+else:
+    print ('Uno is played with multi colored cards') 
+    
+print  ('**********************')
+    
+print ('Genre:Card Game')
+
+print  ('**********************')
+
+print ('Rating for war:') +str(5)
